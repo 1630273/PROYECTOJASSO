e59d3778208a6c58e20ae6d0c52ccdf08ba3bdf8
1630273 <1630273@upv.edu.mx>
2019-07-18 01:43:21 -0500

    ejemplo de agregar y conexion bd

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -5,8 +5,8 @@ $cons_contra="";
 $cons_base_datos="PWJ";
 $cons_equipo="localhost";
 
-$obj_conexion = mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
-if(!$obj_conexion)
+$conexion = mysqli_connect($cons_equipo,$cons_usuario,$cons_contra,$cons_base_datos);
+if(!$conexion)
 {
     echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
 }
--- pages/registro.php
+++ pages/registro.php
@@ -1,6 +1,29 @@
 <?php
     include("../conexion/conexion.php");
 
+    if(isset($_POST['registrar'])){
+        if(strlen($_POST['matricula'])>=1 && strlen($_POST['nombre'])>=1 && strlen($_POST['AP'])>=1 && strlen($_POST['nombre'])>=1){
+            $matricula= trim( $_POST['matricula']);
+            $nombre= trim( $_POST['nombre']);
+            $AP= trim( $_POST['AP']);
+            $AM= trim( $_POST['AM']);
+            $consulta = "INSERT INTO MAESTROS(matricula,nombre,apellidoP,apellidoM) VALUES('$matricula','$nombre','$AP','$AM')";
+
+            $resultado = mysqli_query($conexion,$consulta);
+            
+            if($resultado){
+                ?>
+                <h3>Registro exitoso</h3>
+                <?php
+            } else {
+                ?>
+                <h3>HA ocurrido un error</h3>
+                <?php
+            }
+
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -12,15 +35,18 @@
     <title>REGISTRO</title>
 </head>
 <body>
-    <form action="POST">
+    <form method="post">
         <h1>REGISTRO</h1>
-       
+        <input type="text" name="matricula" placeholder="Matricula">
         <input type="text" name="nombre" placeholder="Nombres">
         <input type="text" name="AP" placeholder="Apellido Patero">
         <input type="text" name="AM" placeholder="Apellido Materno">
         <input type="submit" name="registrar" >
     
     </form>
+
+
+    
     
 </body>
 </html>
